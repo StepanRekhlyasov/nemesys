3a5e1155703ad7ca1ebef0fd190a2af48ea4e7d3
kumar398 <ankur.kumar@opus.co.jp>
2022-09-26 18:54:57 +0900

    pushing ts code

--- src/helpers/firebaseConfig.ts
+++ src/helpers/firebaseConfig.ts
@@ -1,9 +1,9 @@
 export const config = {
-  apiKey: 'AIzaSyDty4AXmQF9uzYdIdz99WJxBxv3sCtEY3c',
-  authDomain: 'dev-nemesys-firebase.firebaseapp.com',
-  projectId: 'dev-nemesys-firebase',
-  storageBucket: 'dev-nemesys-firebase.appspot.com',
-  messagingSenderId: '57116370384',
-  appId: '1:57116370384:web:68905106593e517d6185d5',
-  measurementId: 'G-RZ9EXY6FFF',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
