63627bfb9eda70e0f7d837a09a1a40c38b895530
ankur.kumar <ankur.kumar@opus.co.jp>
2022-11-09 15:53:10 +0530

    adding prefecture list

--- src/pages/applicant/add.vue
+++ src/pages/applicant/add.vue
@@ -10,122 +10,124 @@
       <q-card-section class="bg-grey-3">
 
         <q-form ref="applicantForm" @submit="onSubmit" @reset="onReset">
-
           <div class="row">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-6">
+          <div class="row">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.name') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['name']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.kanaName') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['kanaName']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.postCode') }}
             </div>
-            <div class="col-3 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['postCode']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.prefecture') }}
             </div>
-            <div class="col-3 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-select outlined dense v-model="applicantData['prefecture']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.municipalities') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['municipalities']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.street') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['street']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.apartment') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['apartment']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.phone') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['phone']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.email') }}
             </div>
-            <div class="col-4 q-pl-sm">
+            <div class="col-8 q-pl-sm">
               <q-input outlined dense v-model="applicantData['email']" bg-color="white" />
             </div>
           </div>
 
+        </div>
+        <div class="col-6">
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.status') }}
             </div>
-            <div class="col-3 q-pl-sm">
+            <div class="col-6 q-pl-sm">
               <q-select outlined dense v-model="applicantData['status']" bg-color="white" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.branchIncharge') }}
             </div>
-            <div class="col-3 q-pl-sm">
+            <div class="col-6 q-pl-sm">
               <q-select outlined dense v-model="applicantData['status']" bg-color="white" />
             </div>
           </div>
 
-          <div class="row q-pt-md q-pb-sm ">
-            <div class="col-2 text-right self-center q-pr-sm">
+          <div class="row q-pt-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.sex') }}
             </div>
-            <div class="col-6 q-pl-sm">
-              <q-radio dense v-model="applicantData['sex']" val="male" :label="$t('applicant.add.male')" />
-              <q-radio dense class="q-pl-md" v-model="applicantData['sex']" val="female"
-                :label="$t('applicant.add.female')" />
+            <div class="col-8 q-pl-sm">
+              <q-radio v-model="applicantData['sex']" val="male" :label="$t('applicant.add.male')" />
+              <q-radio v-model="applicantData['sex']" val="female" :label="$t('applicant.add.female')" />
             </div>
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.dob') }}
             </div>
-            <div class="col-3 q-pl-sm">
+            <div class="col-6 q-pl-sm">
               <q-input dense outlined bg-color="white" v-model="applicantData['dob']">
                 <template v-slot:append>
                   <q-icon name="event" class="cursor-pointer">
@@ -142,28 +144,28 @@
             </div>
           </div>
 
-          <div class="row q-pt-md q-pb-sm ">
-            <div class="col-2 text-right self-center q-pr-sm">
+          <div class="row q-pt-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.occupation') }}
             </div>
-            <div class="col-10 q-pl-sm">
-              <q-radio dense v-model="applicantData['occupation']" val="nurse" :label="$t('applicant.add.nurse')" />
-              <q-radio dense class="q-pl-md" v-model="applicantData['occupation']" val="nursingCare"
+            <div class="col-9 q-pl-sm">
+              <q-radio v-model="applicantData['occupation']" val="nurse" :label="$t('applicant.add.nurse')" />
+              <q-radio v-model="applicantData['occupation']" val="nursingCare"
                 :label="$t('applicant.add.nursingCare')" />
-              <q-radio dense class="q-pl-md" v-model="applicantData['occupation']" val="lifeCounselor"
+              <q-radio v-model="applicantData['occupation']" val="lifeCounselor"
                 :label="$t('applicant.add.lifeCounselor')" />
-              <q-radio dense class="q-pl-md" v-model="applicantData['occupation']" val="careManager"
+              <q-radio v-model="applicantData['occupation']" val="careManager"
                 :label="$t('applicant.add.careManager')" />
-              <q-radio dense class="q-pl-md" v-model="applicantData['occupation']" val="others"
+              <q-radio  v-model="applicantData['occupation']" val="others"
                 :label="$t('applicant.add.others')" />
             </div>
           </div>
 
           <div class="row q-pt-md q-pb-sm ">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.qualification') }}
             </div>
-            <div class="col-10 q-pl-sm">
+            <div class="col-9 q-pl-sm">
               <q-checkbox dense v-model="applicantData['qualification']" val="registeredNurse"
                 :label="$t('applicant.add.registeredNurse')" />
               <q-checkbox dense v-model="applicantData['qualification']" val="assistantNurse"
@@ -176,10 +178,10 @@
           </div>
 
           <div class="row q-pt-sm">
-            <div class="col-2 text-right self-center q-pr-sm">
+            <div class="col-3 text-right self-center q-pr-sm">
               {{ $t('applicant.add.applicationDate') }}
             </div>
-            <div class="col-3 q-pl-sm">
+            <div class="col-6 q-pl-sm">
               <q-input dense outlined bg-color="white" v-model="applicantData['applicationDate']">
                 <template v-slot:prepend>
                   <q-icon name="event" class="cursor-pointer">
@@ -209,7 +211,8 @@
             </div>
           </div>
 
-
+        </div>
+      </div>
           <q-separator color="white" size="2px" class="q-mt-md" />
 
           <div class="q-pt-sm">
