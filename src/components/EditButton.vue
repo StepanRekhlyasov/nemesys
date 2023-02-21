d24a3f13e057534585f2f614320496fda553310d
Pilipovi Vitali <pilipovi.vitali@opus.co.jp>
2023-02-21 16:52:50 +0400

    31965: fix user editing bug, separate edit button

---
+++ src/components/EditButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-td :props="props" auto-width>
+    <q-btn v-if="props.rowIndex !== editableRow" icon="edit" flat
+      @click="emit('onEditableRowChange', props.rowIndex); onEdit();" :color="color" />
+    <q-btn v-if="editableRow >= 0 && props.rowIndex === editableRow" flat icon="mdi-content-save"
+      @click="onSave(); emit('onEditableRowChange', -1)" :color="color" />
+  </q-td>
+</template>
+
+<script setup lang="ts">
+import { QTableSlots } from 'quasar'
+
+interface EditButtonProps {
+  props: Parameters<QTableSlots[`body-cell-${string}`]>[0]
+  color: string
+  editableRow: number
+  onEdit: () => void
+  onSave: () => Promise<void>
+}
+
+defineProps<EditButtonProps>()
+
+const emit = defineEmits<{ (e: 'onEditableRowChange', value: number): void }>();
+
+</script>
