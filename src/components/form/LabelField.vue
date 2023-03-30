a67cd0bd97b6f8179dee4c2e3e6d6fb8b7808091
Veysman Nikita <veysmanns@it2g.ru>
2023-03-30 12:11:58 +0300

    Add field label component

---
+++ src/components/form/LabelField.vue
@@ -0,0 +1,42 @@
+<template>  
+  <div class="col-3 q-pl-md text-right text-blue text-weight-regular self-center">
+    {{ label }}
+  </div>
+  <div :class="['q-pl-md blue', valueClass]">
+    <hidden-text v-if="!edit" class="text_dots" :value="value" />
+    <template v-if="edit">
+      <slot>
+      </slot>
+    </template>
+  </div>
+</template> 
+
+<script lang="ts" >
+import hiddenText from 'src/components/hiddingText.component.vue';
+export default {
+  components: {
+    hiddenText
+  },
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    edit: {
+      type: Boolean,
+      required: true
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: String
+    },
+    valueClass: {
+      type: String,
+      default: 'col-3'
+    }
+  }
+}
+</script>
