efa94f5c982309df9f71cb382943996fdaf6a447
Veysman Nikita <veysmanns@it2g.ru>
2023-04-21 12:12:25 +0300

    router update

--- src/router/backOrder.router.ts
+++ src/router/backOrder.router.ts
@@ -9,14 +9,7 @@ const backOrder_router: MenuRouter[] = [
 			requiresAuth: true,
 		},
 		menuParent: MenuParent.BackOrder,
-	},
-	{
-		path: 'backOrder/list',
-		title: 'menu.savedSearchConditionsList',
-		component: () => import('src/pages/user/Applicant/add.vue'),
-		meta: { requiresAuth: true },
-		menuParent: MenuParent.BackOrder
-	},
+	}
 ]
 
 export default backOrder_router;
