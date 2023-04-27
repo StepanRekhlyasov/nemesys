3974df599072db5114ddf93b443a897774ee8c88
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2023-04-28 00:32:44 +0400

    admin/ new client and head client factory

--- src/pages/admin/OfficeManagment/ClientFactory.vue
+++ src/pages/admin/OfficeManagment/ClientFactory.vue
@@ -1,21 +1,27 @@
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useAdminClientFactory } from 'src/stores/admin/clientFactory';
+import CFPageActions from './components/ClientFactory/CFPageActions.vue';
 import ClientFactoryDrawer from './ClientFactoryDrawer.vue';
+import NewClientDrawer from './NewClientDrawer.vue';
 import ClientFactoryTable from './components/ClientFactory/ClientFactoryTable.vue';
 import Pagination from './components/PaginationView.vue';
 import { ClientFactory } from 'src/shared/model/ClientFactory.model';
 import { ClientFactoryTableRow } from './types';
 import { clientFactoriesToTableRows } from './handlers/ClientFactory';
+import { useAdminClient } from 'src/stores/admin/client';
 
 const { t } = useI18n({ useScope: 'global' });
-const adminClientFactory = useAdminClientFactory()
+const { clientFactories,  getClientFactories } = useAdminClientFactory()
+const { clients } = useAdminClient()
 
 const activeClientFactoryItem = ref<ClientFactory | null>(null)
 const tableRows = ref<ClientFactoryTableRow[]>([])
 const fetchData = ref(false)
 const isClientFactoryDrawer = ref(false)
+const isNewClientDrawer = ref(false)
+
 const pagination = ref({
     sortBy: 'desc',
     descending: false,
@@ -27,26 +33,35 @@ const pagination = ref({
 const clientFactoryDrawerHandler = (item: ClientFactoryTableRow) => {
     isClientFactoryDrawer.value = false
 
-    activeClientFactoryItem.value = adminClientFactory.clientFactories.find((factory) => factory.id === item.id) as ClientFactory
+    activeClientFactoryItem.value = clientFactories.find((factory) => factory.id === item.id) as ClientFactory
 
     if(activeClientFactoryItem.value) {
         isClientFactoryDrawer.value = true
     }
 }
 
-const hideDrawer = () => {
+const hideClientFactoryDrawer = () => {
     isClientFactoryDrawer.value = false
 }
 
-onMounted(() => {
-    fetchData.value = true
-    adminClientFactory.getClientFactories()
-        .then((factoriesData) => {
-            const rows = clientFactoriesToTableRows(factoriesData)
-            tableRows.value = rows
-            fetchData.value = false
-        })
-})
+const hideNewClientDrawer = () => {
+    isNewClientDrawer.value = false
+}
+const openNewClientDrawer = () => {
+    isNewClientDrawer.value = true
+}
+
+const updateTableRows = (factories: ClientFactory[]) => {
+    tableRows.value = []
+    tableRows.value = clientFactoriesToTableRows(factories)
+    fetchData.value = false
+}
+
+watch(clients, async () => {
+    fetchData.value = true;
+    const fetchedClientFactories = await getClientFactories(clients.value);
+    updateTableRows(fetchedClientFactories);
+}, {immediate: true});
 </script>
 
 <template>
@@ -56,29 +71,7 @@ onMounted(() => {
                 <div class="title text-h6 text-weight-bold">{{ t('menu.admin.masterSearch') }}</div>
             </q-card-section>
             <q-separator color="grey-4" size="2px" />
-            <q-card-section class="bg-white">
-                <div class="flexRowBetween">
-                    <div class="text-subtitle1">
-                        {{ t('common.searchCondition') }} / 
-                        {{ t('common.area') }} : 
-                        {{ t('common.aroundTokio') }}, 
-                        {{ t('common.detailedConditions') }}
-                    </div>
-                    <div>
-                        <a class="action_link">
-                            {{ t('actions.clickToAddNewClient') }}
-                        </a>
-                    </div>
-                </div>
-                <div class="flexRowBetween q-mt-sm">
-                    <q-btn class="action_btn">
-                        {{ t('actions.searchConditionChange') }}
-                    </q-btn>
-                    <q-btn icon="add" class="action_btn">
-                        {{ t('menu.addOffice') }}
-                    </q-btn>
-                </div>
-            </q-card-section>
+            <CFPageActions @open-drawer="openNewClientDrawer"/>
             <q-card-section class="table no-padding">
                 <ClientFactoryTable
                 @select-item="clientFactoryDrawerHandler"
@@ -96,7 +89,12 @@ onMounted(() => {
         v-if="activeClientFactoryItem"
         v-model:selectedItem="activeClientFactoryItem"
         :isDrawer="isClientFactoryDrawer"
-        @hide-drawer="hideDrawer"/>
+        @hide-drawer="hideClientFactoryDrawer"/>
+
+        <NewClientDrawer
+        @hide-drawer="hideNewClientDrawer"
+        theme="accent"
+        :isDrawer="isNewClientDrawer" />
     </div>
 </template>
 
@@ -109,21 +107,5 @@ onMounted(() => {
 }
 .pagination {
     padding: 2% 2%;
-}
-.flexRowBetween {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-}
-.action_btn {
-    background-color: $main_purple;
-    color: white;
-    font-weight: bold;
-}
-.action_link {
-    text-decoration: underline;
-    line-height: 14px;
-    color: $main_purple;
 }
 </style>
