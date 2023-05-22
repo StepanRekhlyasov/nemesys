3d54d3968a42148f219977d2b11dc1aa6512b134
Stepan <srekhlyasov@gmail.com>
2023-05-22 11:42:44 +0300

    select component

--- src/pages/user/ApplicantProgress/ApplicantProgress.vue
+++ src/pages/user/ApplicantProgress/ApplicantProgress.vue
@@ -8,47 +8,26 @@
       <div class="row q-pt-md q-gutter-sm">
         <div class="col-2">
           <p class="q-ml-md">{{ $t("applicant.progress.filters.branch") }}</p>
-          <q-select
-            outlined
-            dense
-            :options="[]"
+          <MySelect 
+            @update="fetchResults()" 
             v-model="applicantStore.state.applicantFilter['branch']"
-            bg-color="white"
-            :label="$t('common.pleaseSelect')"
-            emit-value
-            map-options
-            clearable
+            :options="[]"
           />
         </div>
         <div class="col-2">
           <p class="q-ml-md">{{ $t("applicant.progress.filters.userInCharge") }}</p>
-          <q-select
-            outlined
-            dense
-            :options="usersInChargeOptions"
+          <MySelect 
+            @update="fetchResults()" 
             v-model="applicantStore.state.applicantFilter['attendeeUserInCharge']"
-            bg-color="white"
-            :label="$t('common.pleaseSelect')"
-            @update:model-value="fetchResults()"
-            emit-value
-            map-options
-            clearable
-            :disable="loading"
+            :options="usersInChargeOptions"
           />
         </div>
         <div class="col-1">
           <p class="q-ml-md">{{ $t("applicant.progress.filters.prefecture") }}</p>
-          <q-select
-            outlined
-            dense
-            :options="prefectureList"
+          <MySelect 
+            @update="fetchResults()" 
             v-model="applicantStore.state.applicantFilter['prefecture']"
-            bg-color="white"
-            :label="$t('common.pleaseSelect')"
-            emit-value
-            map-options
-            @update:model-value="fetchResults()"
-            clearable
+            :options="prefectureList"
           />
         </div>
         <div class="col-2">
@@ -95,72 +74,73 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed, onMounted, ref, watch } from 'vue';
-  import ApplicantColumn from './components/ApplicantColumn.vue';
-  import { APPLICANT_COLUMNS } from './const/applicantColumns';
-  import { useApplicant } from 'src/stores/applicant';
-  import { COLUMN_STATUSES, COUNT_STATUSES } from './const/applicantColumns';
-  import { limitQuery } from './const/applicantColumns';
-  import ApplicantDetails from '../Applicant/ApplicantDetails.vue';
-  import { prefectureList } from 'src/shared/constants/Prefecture.const';
-  import YearMonthPicker from 'src/components/inputs/YearMonthPicker.vue';
-  
-  /** consts */
-  const detailsDrawer = ref<InstanceType<typeof ApplicantDetails> | null>(null)
-  const perQuery = ref<number>(limitQuery)
-  const countApplicantsStatuses = ref({
-    entry: 0,
-    retired: 0,
-    working: 0
-  })
-  const columns = computed(()=>APPLICANT_COLUMNS.map(item => {
-    return {...item,
-      items: applicantsByColumn.value[item.status]
+import { computed, onMounted, ref, watch } from 'vue';
+import ApplicantColumn from './components/ApplicantColumn.vue';
+import { APPLICANT_COLUMNS } from './const/applicantColumns';
+import { useApplicant } from 'src/stores/applicant';
+import { COLUMN_STATUSES, COUNT_STATUSES } from './const/applicantColumns';
+import { limitQuery } from './const/applicantColumns';
+import ApplicantDetails from '../Applicant/ApplicantDetails.vue';
+import YearMonthPicker from 'src/components/inputs/YearMonthPicker.vue';
+import MySelect from 'src/components/inputs/MySelect.vue';
+import { prefectureList } from 'src/shared/constants/Prefecture.const';
+
+/** consts */
+const detailsDrawer = ref<InstanceType<typeof ApplicantDetails> | null>(null)
+const perQuery = ref<number>(limitQuery)
+const countApplicantsStatuses = ref({
+  entry: 0,
+  retired: 0,
+  working: 0
+})
+const columns = computed(()=>APPLICANT_COLUMNS.map(item => {
+  return {...item,
+    items: applicantsByColumn.value[item.status]
+  }
+}))
+const columnsLoading = computed(() => applicantStore.state.columnsLoading);
+const loading = computed(()=>{
+  for (const value of Object.values(columnsLoading.value)){
+    if(!value){
+      return false
     }
-  }))
-  const columnsLoading = computed(() => applicantStore.state.columnsLoading);
-  const loading = computed(()=>{
-    for (const value of Object.values(columnsLoading.value)){
-      if(!value){
-        return false
-      }
+  }
+  return true
+})
+const usersInChargeOptions = computed(()=>{
+  return applicantStore.state.usersInCharge.map((doc) => {
+    return {
+      label: doc.displayName,
+      value: doc.id
     }
-    return true
-  })
-  const usersInChargeOptions = computed(()=>{
-    return applicantStore.state.usersInCharge.map((doc) => {
-      return {
-        label: doc.displayName,
-        value: doc.id
-      }
-    });
   });
+});
 
-  /** stores */
-  const applicantStore = useApplicant();
+/** stores */
+const applicantStore = useApplicant();
 
-  /** getters */
-  const applicantsByColumn = computed(() => applicantStore.state.applicantsByColumn);
+/** getters */
+const applicantsByColumn = computed(() => applicantStore.state.applicantsByColumn);
 
-  /** fetchers */
-  const fetchResultsHandler = async (status : string, fetchMore = false) => {
-    columnsLoading.value[status] = true
-    await applicantStore.getApplicantsByStatus(status, applicantStore.state.applicantFilter, perQuery.value, fetchMore)
-    columnsLoading.value[status] = false
-  }
-  const fetchResults = async () => {
-    COLUMN_STATUSES.map(async (status)=>{
-      fetchResultsHandler(status, false)
-    })
-    COUNT_STATUSES.map(async (status)=>{
-      countApplicantsStatuses.value[status] = await applicantStore.countApplicantsByStatus(status, applicantStore.state.applicantFilter)
-    })
-  }
-  onMounted( async ()=>{
-    applicantStore.fetchUsersInChrage()
-    await fetchResults()
+/** fetchers */
+const fetchResultsHandler = async (status : string, fetchMore = false) => {
+  columnsLoading.value[status] = true
+  await applicantStore.getApplicantsByStatus(status, applicantStore.state.applicantFilter, perQuery.value, fetchMore)
+  columnsLoading.value[status] = false
+}
+const fetchResults = async () => {
+  COLUMN_STATUSES.map(async (status)=>{
+    fetchResultsHandler(status, false)
   })
-  watch(()=>applicantStore.state.applicantFilter['currentStatusMonth'], (newVal, oldVal)=>{
-    if(newVal!=oldVal) fetchResults()
+  COUNT_STATUSES.map(async (status)=>{
+    countApplicantsStatuses.value[status] = await applicantStore.countApplicantsByStatus(status, applicantStore.state.applicantFilter)
   })
+}
+onMounted( async ()=>{
+  applicantStore.fetchUsersInChrage()
+  await fetchResults()
+})
+watch(()=>applicantStore.state.applicantFilter['currentStatusMonth'], (newVal, oldVal)=>{
+  if(newVal!=oldVal) fetchResults()
+})
 </script>
