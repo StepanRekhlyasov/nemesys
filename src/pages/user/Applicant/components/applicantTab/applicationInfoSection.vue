6ae99f76fd0e860c851ce05c895745ab574a07a1
Veysman Nikita <veysmanns@it2g.ru>
2023-06-06 15:38:29 +0300

    close 32536

--- src/pages/user/Applicant/components/applicantTab/applicationInfoSection.vue
+++ src/pages/user/Applicant/components/applicantTab/applicationInfoSection.vue
@@ -15,23 +15,11 @@
           <template v-slot:prepend>
             <q-icon name="event" class="cursor-pointer">
               <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-date v-model="data['applicationDate']" mask="YYYY/MM/DD HH:mm">
+                <q-date v-model="data['applicationDate']" mask="YYYY/MM/DD">
                   <div class="row items-center justify-end">
                     <q-btn v-close-popup label="Close" color="primary" flat />
                   </div>
                 </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-
-          <template v-slot:append>
-            <q-icon name="access_time" class="cursor-pointer">
-              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-time v-model="data['applicationDate']" mask="YYYY/MM/DD HH:mm" format24h>
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Close" color="primary" flat />
-                  </div>
-                </q-time>
               </q-popup-proxy>
             </q-icon>
           </template>
