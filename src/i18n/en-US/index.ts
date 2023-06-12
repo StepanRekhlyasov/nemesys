d9f550322d24f9b318da3af1825e26fb11a719eb
Stepan <srekhlyasov@gmail.com>
2023-06-12 06:46:24 +0300

    kpi table UI

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -87,7 +87,8 @@ export default {
       '11' : 'November',
       '12' : 'December',
     },
-    more: 'More'
+    more: 'More',
+    downloadCSV: 'Download CSV'
   },
   smoking: {
     no: 'No',
@@ -146,6 +147,7 @@ export default {
     addNewClient: 'Click here to add a new client',
     addClient: 'Add new Client',
     addClientHint: 'Click here if you want to add a new office or client',
+    KPI: 'KPI',
     admin: {
       masterSearch: 'Office master search',
       officeManagement: 'Office management',
@@ -1662,5 +1664,19 @@ export default {
     showWhiteboard: 'Show whiteboard',
     showList: 'Show list',
     waitList: ' waiting list'
+  },
+  kpi: {
+    targetPeriod: 'Target period',
+    numberOfFix: '№ of FIX',
+    jobs: 'Jobs',
+    innerConstant: 'Inner constant',
+    entry: 'Entry',
+    numberOfCalls: '№ of Calls',
+    numberOfFax: '№ of FAX',
+    dispatch: 'Dispatch',
+    introduction: 'Introduction',
+    occupationRate: 'Occupation rate',
+    jobOfferRate: 'Job offer rate',
+    hiringRate: 'Hiring rate'
   }
 }
