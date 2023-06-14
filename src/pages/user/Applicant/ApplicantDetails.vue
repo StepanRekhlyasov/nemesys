1aca02bb54e6e886851f608e292ae5de280de836
Veysman Nikita <veysmanns@it2g.ru>
2023-06-14 18:11:01 +0300

    2 bug

--- src/pages/user/Applicant/ApplicantDetails.vue
+++ src/pages/user/Applicant/ApplicantDetails.vue
@@ -26,7 +26,7 @@
               </div>
               <div class="col-3">
                 <span class="row">{{ selectedApplicant.prefecture }} {{ selectedApplicant.municipalities }} </span>
-                <span class="row">{{ selectedApplicant.street }} {{ selectedApplicant.apartament }}</span>
+                <span class="row">{{ selectedApplicant.street }} {{ selectedApplicant.apartment }}</span>
               </div>
             </div>
             <div class="row">
