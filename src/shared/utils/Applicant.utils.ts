bae0902b3d66ec7f43226c09b57bc9283263851b
Stepan <srekhlyasov@gmail.com>
2023-07-13 07:24:01 +0300

    review fix

--- src/shared/utils/Applicant.utils.ts
+++ src/shared/utils/Applicant.utils.ts
@@ -1,11 +1,5 @@
-import { collection, doc, Firestore,  getDocs, getFirestore, orderBy, query, where, writeBatch } from 'firebase/firestore';
+import { collection, Firestore,  getDocs, getFirestore, orderBy, query, where, writeBatch } from 'firebase/firestore';
 import { ConstraintsType } from './utils';
-import { Applicant } from '../model';
-import { Alert } from './Alert.utils';
-import { ref } from 'vue'
-import { requiredFields } from '../constants/Applicant.const';
-
-const db = getFirestore();
 
 export const getApplicantContactsList = (db: Firestore, applicant_id: string) => {
   return getDocs(query(
@@ -35,49 +29,4 @@ export const getClientFactoriesList = (db: Firestore, client_id: string) => {
     where('deleted', '==', false),
     orderBy('name')
   ))
-}
-
-export async function validateAllApplicants() {
-  const applicantRef = collection(db, 'applicants')
-  const applicantQuery = query(applicantRef)
-  const docSnap = await getDocs(applicantQuery)
-  if (!docSnap.empty) {
-    const documents = docSnap.docs.map(item => {
-      const id = item.id
-      return { id, ...item.data() }
-    })
-    return validateApplicants(documents as Applicant[])
-  }
-  return false
-}
-
-/** this function checks and creates reqiured fields if they would not exist for some reason */
-export async function validateApplicants(applicants: Applicant[]) {
-  const fire = ref(false)
-  const batch = writeBatch(db);
-  const forUpdate: Record<string, string | number>[] = []
-  applicants.map((applicant) => {
-    const needsUpdate: { [key: string]: string | number; } = {}
-    for (const [key, value] of Object.entries(requiredFields.value)) {
-      if (typeof applicant[key] === 'undefined') {
-        needsUpdate[key] = value
-      }
-    }
-    if (Object.keys(needsUpdate).length > 0) {
-      forUpdate[applicant.id] = needsUpdate
-    }
-  })
-  for (const [key, value] of Object.entries(forUpdate)) {
-    fire.value = true
-    const docRef = doc(db, 'applicants/' + key);
-    batch.update(docRef, { ...value })
-  }
-  if (fire.value) {
-    try {
-      await batch.commit()
-    } catch (e) {
-      Alert.warning(e)
-    }
-  }
-  return fire.value
 }
