45069d05a723f43b05ba505fd21a274eb1592b7a
hinoue.koichi <hinoue.koichi@opus.co.jp>
2023-07-19 14:33:31 +0900

    set legend rightside age sex day

--- src/components/report/RecruitmentEffectivenessreport/const.ts
+++ src/components/report/RecruitmentEffectivenessreport/const.ts
@@ -50,7 +50,7 @@ export const unitPricenamesPerMedia = [
 ] as const;
 export const chartOptionsSex = computed(() => {
   return {
-    legend: { position: 'left' },
+    legend: { position: 'right' },
     chart: { stacked: true },
     title: {
       text: t('report.title.sex'),
@@ -92,7 +92,7 @@ export const chartOptionsSex = computed(() => {
 
 export const chartOptionsAges = computed(() => {
   return {
-    legend: { position: 'left' },
+    legend: { position: 'right' },
     chart: { stacked: true },
     title: {
       text: t('report.title.age'),
@@ -133,7 +133,7 @@ export const chartOptionsAges = computed(() => {
 
 export const chartOptionsDaysToWork = computed(() => {
   return {
-    legend: { position: 'left' },
+    legend: { position: 'right' },
     chart: { stacked: true },
     title: {
       text: t('report.title.daysToWork'),
