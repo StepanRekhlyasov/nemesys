d31b5ea5f4f5a4e66d9d1ce1df73a25a1125b44f
hinoue.koichi <hinoue.koichi@opus.co.jp>
2023-07-20 10:25:31 +0900

    switch report page

--- src/components/report/ApplicantReport/ApplicantReport.vue
+++ src/components/report/ApplicantReport/ApplicantReport.vue
@@ -19,33 +19,13 @@
       ></ApplicantReportRightChart>
     </div>
   </div>
-  <div class="row">
-    <div class="col">
-      <ApplicantReportLeftDownChart
-        :organization_id="organization_id"
-        :dateRangeProps="dateRangeProps"
-        :branch_id="branch_id"
-        :branch_user_list="branch_user_list"
-        :graph_type="graph_type"
-      ></ApplicantReportLeftDownChart>
-    </div>
-    <div class="col">
-      <ApplicantReportRightDownChart
-        :organization_id="organization_id"
-        :dateRangeProps="dateRangeProps"
-        :branch_id="branch_id"
-        :branch_user_list="branch_user_list"
-        :graph_type="graph_type"
-      ></ApplicantReportRightDownChart>
-    </div>
-  </div>
+
 </template>
 
 <script setup lang="ts">
 import ApplicantReportLeftChart from './ApplicantReportLeftChart.vue';
 import ApplicantReportRightChart from './ApplicantReportRightChart.vue';
-import ApplicantReportRightDownChart from './ApplicantReportRightDownChart.vue';
-import ApplicantReportLeftDownChart from './ApplicantReportLeftDownChart.vue';
+
 import { graphType } from '../Models';
 
 defineProps<{
--- src/components/report/SalesActivityReport/SalesActivityReport.vue
+++ src/components/report/SalesActivityReport/SalesActivityReport.vue
@@ -8,22 +8,37 @@
       :graph_type="graph_type"
     ></SalesActivityReportUpLeftChart>
   </div>
-  <div class="col">
-    <!-- <SalesActivityReportRightChart :organization_id="organization_id"
-      :dateRangeProps="dateRangeProps"
-      :branch_id="branch_id"
-      :branch_user_list="branch_user_list"></SalesActivityReportRightChart> -->
+  <div class="row">
+    <div class="col">
+      <ApplicantReportLeftDownChart
+        :organization_id="organization_id"
+        :dateRangeProps="dateRangeProps"
+        :branch_id="branch_id"
+        :branch_user_list="branch_user_list"
+        :graph_type="graph_type"
+      ></ApplicantReportLeftDownChart>
+    </div>
+    <div class="col">
+      <ApplicantReportRightDownChart
+        :organization_id="organization_id"
+        :dateRangeProps="dateRangeProps"
+        :branch_id="branch_id"
+        :branch_user_list="branch_user_list"
+        :graph_type="graph_type"
+      ></ApplicantReportRightDownChart>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import SalesActivityReportUpLeftChart from './SalesActivityReportUpLeftChart.vue';
-import {graphType} from '../Models';
-
+import ApplicantReportRightDownChart from './ApplicantReportRightDownChart.vue';
+import ApplicantReportLeftDownChart from './ApplicantReportLeftDownChart.vue';
+import { graphType } from '../Models';
 
 defineProps<{
   branch_id: string;
-  dateRangeProps: { from: string; to: string }|undefined;
+  dateRangeProps: { from: string; to: string } | undefined;
   organization_id: string;
   branch_user_list: { id: string; name: string }[];
   graph_type: graphType;
