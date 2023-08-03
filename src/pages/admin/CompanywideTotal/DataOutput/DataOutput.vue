0818ac073b6c86e71571c88ce664a4447b47b639
gajendra2807 <91866208+gajendra2807@users.noreply.github.com>
2023-08-03 21:09:03 +0530

    fix

--- src/pages/admin/CompanywideTotal/DataOutput/DataOutput.vue
+++ src/pages/admin/CompanywideTotal/DataOutput/DataOutput.vue
@@ -1,35 +1,30 @@
 <template>
-    <PageHader>
-        {{ t('menu.admin.dataOutputDisplay') }}
-    </PageHader>
-    <div class="q-pt-lg q-pl-sm custom-color" v-for="(item, idx) in Listitem" :key="idx">
-        <div class="bold-text">
-            <q-avatar square size="10px" color="purple" class="q-mr-sm"/>
-            <span v-if="!item.required">{{ t('menu.admin.dataOutputDisplayTable.companyWide') }}</span> {{ item.label }} {{
-                t('menu.admin.dataOutputDisplay') }}
-        </div>
-        <div class="box1">
-            <div class="row q-pt-sm" v-if="item.required">
-                <div class="col-2 q-pt-sm q-pl-sm ">
+  <PageHader>
+      {{ t('menu.admin.dataOutputDisplay') }}
+  </PageHader>
+  <div class="q-pt-lg q-pl-sm custom-color" v-for="(item, idx) in Listitem" :key="idx">
+      <div class="bold-text">
+          <q-avatar square size="10px" color="purple" class="q-mr-sm" />
+          <span v-if="!item.required">{{ t('menu.admin.dataOutputDisplayTable.companyWide') }}</span> {{ item.label }} {{
+              t('menu.admin.dataOutputDisplay') }}
+      </div>
+      <div class="box1">
+          <div class="row q-pt-sm" v-if="item.required">
+              <div class="col-1 q-pt-sm q-pl-sm ">
                   {{ t('KPI.targetPeriod') }}
-                </div>
-                <div class="col-3 q-pl-sm">
-                  <DateRange
-          v-model="dateRange"
-          :width="'250px'"
-          :height="'40px'"
-        />
-                </div>
-            </div>
-            <div class="row q-pt-sm q-pl-xl q-ml-md">
-                <div class="col-3 q-pl-xl q-ml-xl">
-                    <q-btn color="purple" text-color="white"
-                        @click="downloadCSV(item.collectionName, dateRange)">{{
-                            t('common.csvOutput') }}</q-btn>
-                </div>
-            </div>
-        </div>
-    </div>
+              </div>
+              <div class="col-1 q-pl-sm">
+                  <DateRange v-model="dateRange" :width="'250px'" :height="'40px'" />
+              </div>
+          </div>
+          <div class="row q-pt-sm q-pl-xs q-ml-sm">
+              <div class="col-3 q-pl-lg q-ml-xl">
+                  <q-btn color="purple" text-color="white" @click="downloadCSV(item.collectionName, dateRange)">{{
+                      t('common.csvOutput') }}</q-btn>
+              </div>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -45,47 +40,37 @@ const triggerURL = process.env.downloadCSVUrl;
 const timeperiod = ref([{ date: '' }, { date: '' }, { date: '' }])
 const dataOutput = useDataOutput();
 const dateRange = ref<
-  string | {
+  {
       from: string;
       to: string;
   } | null>(null)
-const downloadCSV = async (collectionName: string, date:string | { from: string; to: string; } | null) => {
-  debugger
-    let fetchURL = `${triggerURL}?collection=${collectionName}`
-    if (collectionName != 'BO') {
-      if (typeof date === 'string' && date.length === 0) {
-    Alert.warning();
-    return;
-   } else if (typeof date === 'object' && (!date?.from || !date.to)) {
-    Alert.warning();
-    return;
-}
-    let year='';
-    let month='';
 
-    if (typeof date === 'string') {
-    [year, month] = date.split('/');
-    }
-        fetchURL = `${fetchURL}&year=${year}&month=${month}`
-    }
-    await dataOutput.downloadCSV(fetchURL,collectionName)
-    onReset();
+const downloadCSV = async (collectionName: string, date: { from: string; to: string; } | null) => {
+  let fetchURL = `${triggerURL}?collection=${collectionName}`
+  if (typeof date === 'object' && (!date?.from || !date.to)) {
+      Alert.warning();
+      return;
+  }
+  fetchURL = `${fetchURL}&from=${date.from}&to=${date.to}`
+  await dataOutput.downloadCSV(fetchURL, collectionName)
+  onReset();
 }
 const onReset = () => {
-    timeperiod.value.filter(item => {
-        item.date = ''
-    });
+  timeperiod.value.filter(item => {
+      item.date = ''
+  });
 }
 </script>
 <style lang="scss">
 .bold-text {
-    font-weight: bold;
+  font-weight: bold;
 }
 
 .box1 {
-    margin-left: 10vw;
+  margin-left: 10vw;
 }
 
 .custom-color {
-    color: purple
-}</style>
+  color: purple
+}
+</style>
