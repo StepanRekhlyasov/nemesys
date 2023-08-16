5ed7396c0ef9f32248e45bf5e2b89937cce89193
gajendra2807 <91866208+gajendra2807@users.noreply.github.com>
2023-08-17 02:53:31 +0530

    fix

--- src/pages/user/Applicant/components/attendanceTab/DesiredSection.vue
+++ src/pages/user/Applicant/components/attendanceTab/DesiredSection.vue
@@ -19,7 +19,7 @@
             <template v-slot:prepend>
               <q-icon name="event" class="cursor-pointer">
                 <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                  <q-date v-model="data['timeToWork']" mask="YYYY/MM/DD" 
+                  <q-date v-model="data['timeToWork']" mask="YYYY/MM/DD"
                   :navigation-min-year-month="nextMonth"
                   :default-year-month="nextMonth"
                   >
@@ -167,7 +167,7 @@
         <hidden-text v-if="!desiredEdit" :value="applicant.nearestStation" />
 
         <q-select v-if="desiredEdit" outlined v-model="data['nearestStation']" :options="stationData"
-          :disable="!data['route'] || loading" dense use-input input-debounce="0" @filter="filterStation" />
+          :disable="loading" dense use-input input-debounce="0" @filter="filterStation" />
       </div>
     </div>
 
@@ -299,6 +299,7 @@ import { Alert } from 'src/shared/utils/Alert.utils';
 import {useClient} from 'src/stores/client'
 import { useOrganization } from 'src/stores/organization';
 import { Client } from 'src/shared/model';
+import { DocumentData } from 'firebase/firestore';
 
 const props = defineProps<{
   applicant: Applicant,
@@ -323,7 +324,7 @@ const transportationServicesOptions = ref(PossibleTransportationServicesList);
 const defaultData = ref<Partial<ApplicantInputs>>({});
   const data = ref<Partial<ApplicantInputs>>({});
 const routeData = ref([]);
-const stationData = ref([]);
+const stationData:DocumentData = ref([]);
 const meansCommutingOptions = computed(() => [
   { value: 'walk', label: t('applicant.attendant.meansCommutingOptions.walk') },
   { value: 'bicycle', label: t('applicant.attendant.meansCommutingOptions.bicycle') },
@@ -362,16 +363,36 @@ onMounted(async () => {
   routeData.value = await metadataStore.getStationRoutes()
   clients.value =  await clientStore.getClientsByConstraints([where('organizationId', '==', organization.currentOrganizationId)])
   getFacilityTypeOptions()
+  stationData.value = await metadataStore.createStationOptions()
 });
 
 watch(() => data.value['route'], async (newVal) => {
-  if (newVal) {
-    data.value['nearestStation'] = '';
-    stationData.value = [];
+  if (newVal && !data.value['route']?.includes('(')) {
+    data.value['nearestStation'] = ''
+    stationData.value = []
     stationData.value = await metadataStore.getStationByID(newVal)
+  }
+  else if(!data.value['route']?.includes('(')){
+    data.value['nearestStation'] = ''
   }
 }
 )
+watch(
+  () => data.value['nearestStation'],
+  async (newVal) => {
+    if(!data.value['route']?.includes('(') && data.value['route'] ){
+    }
+    else if (newVal) {
+      const routeName = await metadataStore.getRouteByStation(newVal);
+      data.value['route'] = `${newVal}(${routeName})`;
+    }
+    else if(data.value['route']?.includes('(')){
+      data.value['route'] = ''
+    }
+  }
+  ,
+  { deep: true , immediate:true}
+);
 watch(() => desiredEdit.value, (newVal) => {
   if (newVal) {
     data.value['nearestStation'] = props.applicant['nearestStation'];
@@ -440,13 +461,20 @@ const getFacilityTypeOptions = () => {
 };
 
 const filterStation = async (val: string, update) => {
-  if (val === '' && data.value.route) {
+  if(val === '' && data.value['route']?.includes('(') && data.value['route'].includes(')')){
+    stationData.value = await metadataStore.createStationOptions()
+    routeData.value = []
+  }
+  else if (val === '' && data.value.route ) {
     update(async () => {
+      if(data.value['route']?.includes('(') && data.value['route'].includes(')')){
+        stationData.value = await metadataStore.createStationOptions()
+      }
       stationData.value = await metadataStore.getStationByID(data.value['route'] as string)
     })
     return
   }
-  update(() => {
+  update(async() => {
     const needle = val.toLowerCase()
     stationData.value = stationData.value.filter(v => v.toLowerCase().indexOf(needle) > -1)
   })
