24af07bc90f3e41e5406d8d61d39a19fa0d44724
dhruv01234 <1235dhruv@gmail.com>
2023-08-16 15:23:07 +0530

    BOID organization added

--- src/stores/backOrder.ts
+++ src/stores/backOrder.ts
@@ -223,7 +223,7 @@ export const useBackOrder = defineStore('backOrder', () => {
     data['updated_at'] = serverTimestamp();
     data['deleted'] = false;
     data['registrant'] = auth.currentUser?.uid;
-    const snapshot = await getCountFromServer(query(collection(db, '/BO')));
+    const snapshot = await getCountFromServer(query(collection(db, '/BO'), where('organizationId', '==', organization.currentOrganizationId)));
     data['boId'] = snapshot.data().count;
 
     const checkNew = await getDocs(query(collection(db, '/BO'), where('office_id', '==', data.office_id), limit(1)))
