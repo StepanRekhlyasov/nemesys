f0f2df71fa590fe54827ff6ff21595e329ac8a96
Pilipovi Vitali <pilipovi.vitali@opus.co.jp>
2023-08-17 09:51:28 +0400

    fix lint

--- src/stores/clientFactory.ts
+++ src/stores/clientFactory.ts
@@ -313,7 +313,7 @@ export const useClientFactory = defineStore('client-factory', () => {
 
           if(typeof createdAt === 'object'){
             if('seconds' in createdAt  && 'nanoseconds' in createdAt){
-                createdAt = new Timestamp(createdAt.seconds as number, createdAt.nanoseconds as number).toDate()
+                createdAt = new Timestamp(createdAt?.seconds as unknown as number, createdAt?.nanoseconds as unknown as number).toDate()
             }
           }
 
